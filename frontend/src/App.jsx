7ff0730a004ea33b2e5b53b4399860ae867ac88e
Abhishek <Abhishekkange@gmail.com>
2023-12-13 00:47:05 +0530

    React router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,26 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Signup from './assets/components/Signup';
-import Services from './assets/components/Services';
-import Projects from './assets/components/Projects';
-import Login from './assets/components/Login';
-import Academy from './assets/components/Academy';
-import Home from './assets/components/Home';
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Services from './Pages/Services'
+import Projects from './Pages/Projects'
+import Navbar from './components/Navbar';
 
 
 function App() {
- 
-
   return (
     <>
-    <h1>Hello</h1>
-
-<Router>
-      <Switch>
-        <Route path='/Home' element={<Home />} />
-        <Route path='/Projects' element={<Projects />} />
-        <Route path='/Services' element={<Services />} />
-        <Route path='/career' element={<Projects />} />
-        <Route path='/AndroJet Academy' element={<Academy />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-      </Switch>
+    <Navbar/>
+    <Router>
+    
+        <Routes>
+          <Route path='/Projects' element={<Projects />} />
+          <Route path='/Home' element={<Home/>} />
+          <Route path='/Services' element={<Services />} />
+        </Routes>
+    
     </Router>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = ()=>{
+
+    return(
+        <h1>Hello</h1>
+    )
+
+
+
+
+
+
+}
+
+export default Navbar;
